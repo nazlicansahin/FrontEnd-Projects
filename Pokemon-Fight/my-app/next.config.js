af944f399e4fbe34ddada042fd99d6c2e18fabf3
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2023-10-25 16:04:57 +0300

    do image config

--- Pokemon-Fight/my-app/next.config.js
+++ Pokemon-Fight/my-app/next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+
+    reactStrictMode: true,
+}
 
 module.exports = nextConfig
+// next.config.js
+
+
+module.exports = {
+    images: {
+        domains: ['images.unsplash.com'],
+    },
+}

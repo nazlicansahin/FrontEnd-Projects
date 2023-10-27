b3a0c50d9a95903e915d2b55541e745a39ec7990
Nazlıcan Şahin <76625343+nazlicansahin@users.noreply.github.com>
2023-10-27 03:30:41 +0300

    Game 1 done

---
+++ Pokemon-Fight/my-app/app/Components/PokemonData.jsx
@@ -0,0 +1,20 @@
+import pikachu from '../../public/pikachu.jpeg'
+import charizard from '../../public/charizard.jpeg'
+import squirtle from '../../public/squirtle.jpeg'
+import bulbasaur from '../../public/bulbasaur.jpeg'
+import jigglypuff from '../../public/jigglypuff.jpeg'
+
+
+
+
+const PokemonData = [
+    { power: 90, name: "Pikachu", imageSrc: pikachu },
+    { power: 120, name: "Charizard", imageSrc: charizard },
+    { power: 60, name: "Squirtle", imageSrc: squirtle },
+    { power: 80, name: "Bulbasaur", imageSrc: bulbasaur },
+    { power: 70, name: "Jigglypuff", imageSrc: jigglypuff },
+    // Add more Pokémon data here
+  ];
+  
+  export default PokemonData;
+  
--- Pokemon-Fight/my-app/app/page.js
+++ Pokemon-Fight/my-app/app/page.js
@@ -1,73 +1,81 @@
-"use client"
+ "use client"
 import 'bootstrap/dist/css/bootstrap.css'
+import startImage from '../public/images.png'
+const startImg =  [  { power: 0, name: "Start", imageSrc: startImage }]
+import React, { useState } from "react";
+import PokemonFight from "@/app/Components/PokemonFight";
+import PokemonData from "./Components/PokemonData"; // Assuming you have a separate file with Pokemon data
 
-import Image from 'next/image'
-import pikachu from '../public/pikachu.jpeg'
-import charizard from '../public/charizard.jpeg'
-import squirtle from '../public/squirtle.jpeg'
-import bulbasaur from '../public/bulbasaur.jpeg'
-import jigglypuff from '../public/jigglypuff.jpeg'
-import { useState } from "react";
-import PokemonFight from "@/app/Components/PokemonFight"
-
-//layout='fill' objectFit='cover'
-
-function fight(pokemon1, pokemon2){
-  if(pokemon1.power > pokemon2.power){
-      return pokemon1
-  }else{
-      return pokemon2
+const fight = (pokemon1, pokemon2) => {
+  if (pokemon1.power > pokemon2.power) {
+    return pokemon1;
+  } else {
+    return pokemon2;
   }
 }
-
-const Pokemon = [
-  { power: 90, name: "Pikachu", imageSrc: pikachu },
-  { power: 120, name: "Charizard", imageSrc: charizard },
-  { power: 60, name: "Squirtle", imageSrc: squirtle },
-  { power: 80, name: "Bulbasaur", imageSrc: bulbasaur },
-  { power: 70, name: "Jigglypuff", imageSrc: jigglypuff },
-]
-
-
-
 export default function Home() {
+  const [currentStatus, setCurrentStatus] = useState(null);
+  const [player1Results, player1SetResults] = useState([startImg[0]]);
+  const [player2Results, player2SetResults] = useState([startImg[0]]);
+  const [currentSet, setCurrentSet] = useState(1);
+  const [gameWinner, setGameWinner] = useState(null);
 
+  const getRandomElFromArr = (arr) => {  // Get a random element from an array
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const randomItem = arr[randomIndex];
+    return randomItem;
+  };
 
-const [currentStatus, setCurrentStatus] = useState(null);
-const [player1Results, player1SetResults] = useState([]);
-const [player2Results, player2SetResults] = useState([]);
-const getRandomElFromArr = (arr) => {
-  const randomIndex = Math.floor(Math.random() * arr.length);
-  const randomItem = arr[randomIndex];
-  return randomItem;
-};
-
-const start = (result, setResult) => {
-  const randomElement = getRandomElFromArr(Pokemon);
-  setTimeout(() => {
-    setCurrentStatus(randomElement);
-    setResult([...result, randomElement]);
-  }, 1000);
-};
-
-
+  const start = (result, setResult) => {
+    const randomElement = getRandomElFromArr(PokemonData);
+    setTimeout(() => {
+      setCurrentStatus(randomElement);
+      setResult([...result, randomElement]);
+      if(player1Results.length === 1 && player2Results.length === 2){
+        const winner = fight(player1Results, player2Results);
+        if(winner === player1Results){
+          setGameWinner("Player 1");
+        }
+        else if(player1Results === player2Results){
+          setGameWinner("Draw");
+        }
+        else{
+          setGameWinner("Player 2");
+        }
+      }
+    }, 1000);
+  };
 
   return (
-<> 
-<button  type="button" class="btn btn-warning" onClick={start(player1Results, player1SetResults)}>Fight!</button>
-<button  type="button" class="btn btn-warning" onClick={start(player2Results, player2SetResults)}>Fight!</button>
+    <>
+      <button
+        type="button"
+        className="btn btn-warning"
+        onClick={() => start(player1Results, player1SetResults)}
+      >
+       Player 1
+      </button>
+      <button
+        type="button"
+        className="btn btn-warning"
+        onClick={() => start(player2Results, player2SetResults)}
+      >
+        Player 2
+      </button>
 
-{player1Results.map((pokemon,index) => {
- return <PokemonFight key={index} pokemon={pokemon} />
-}
-)
-}
-
-{player2Results.map((pokemon,index) => {
- return <PokemonFight key={index} pokemon={pokemon} />
-}
-)
-}
-</>
-  )
+       <button
+          type="button"
+          className="btn btn-info"
+          onClick={() => newGame()}
+        >
+          New Game
+        </button>
+        <PokemonFight pokemon={player1Results[player1Results.length-1]} />
+   
+        <PokemonFight pokemon={player2Results[player2Results.length-1]} />
+      
+      <p>Set: {currentSet}</p>
+      <p>Game Winner: {gameWinner}</p>
+    </>
+  );
 }
